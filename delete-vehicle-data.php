ca5f9a0f71b9af6cb38df093868edca2af7c43a2
GunaRakulan-GR <gunarakulan@gmail.com>
2022-06-23 20:33:08 +0530

    Add vehicle delete function

---
+++ delete-vehicle-data.php
@@ -0,0 +1,20 @@
+<?php
+
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "feul-management-system-database";
+
+$auto_id = $_GET['id'];
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+$sql = "DELETE FROM vehicle_data WHERE auto_id=$auto_id";
+
+if ($conn->query($sql) === TRUE) {
+    header('Location: register-view.php');
+} else {
+  echo "Error deleting record: " . $conn->error;
+}
+
+?>
--- register-view.php
+++ register-view.php
@@ -100,7 +100,7 @@ $result = $conn -> query($sql);
                                 echo "<td>".$row["vehicle_number"]."</td>";
                                 echo "<td>".$row["vehicle_owner_name"]."</td>";
                                 echo "<td>".$row["licence_renewal_status"]."</td>";
-                                echo "<td>".'<a class="btn btn-success btn-sm">Edit</a> <a class="btn btn-danger btn-sm">Delete</a>'."</td>";
+                                echo "<td>".'<a href="edit-vehicle-data.php?id='.$row["auto_id"].'" class="btn btn-success btn-sm">Edit</a> <a href="delete-vehicle-data.php?id='.$row["auto_id"].'" class="btn btn-danger btn-sm">Delete</a>'."</td>";
                                 echo "</tr>";
 
                                 $index++;
