2d5bf768787781ef18f85f95c2250923fd9e7f3f
GunaRakulan-GR <gunarakulan@gmail.com>
2022-07-25 12:02:54 +0530

    Fix edit function

--- edit-model.php
+++ edit-model.php
@@ -11,10 +11,11 @@ $vehicle_no = $_POST['vehicle_no'];
 $vehicle_owner = $_POST['vehicle_owner'];
 $licence_status = $_POST['licence_status'];
 
+
 // Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
 
-$sql = "UPDATE vehicle_data SET vehicle_number='$vehicle_no', vehicle_owner_name = '$vehicle_owner', licence_renewal_status = '$licence_renewal_status' WHERE auto_id = $auto_id";
+$sql = "UPDATE vehicle_data SET vehicle_number='$vehicle_no', vehicle_owner_name = '$vehicle_owner', licence_renewal_status = '$licence_status' WHERE auto_id = '$auto_id'";
 
 if ($conn->query($sql) === TRUE) {
    header('Location: register-view.php');
